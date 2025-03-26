382fb08b60c0d506322d2321f7e16ac296c6e50d
manish-rajput-1988 <manish.rajput.8819@gmail.com>
2025-03-26 21:35:39 +0530

    Update locals.tf

--- modules/eks/locals.tf
+++ modules/eks/locals.tf
@@ -3,7 +3,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 data "aws_eks_cluster" "eks-cluster" {
-  name = aws_eks_cluster.eks-cluster.name
+  name = data.aws_eks_cluster.eks-cluster.name
 }
 
 locals {
