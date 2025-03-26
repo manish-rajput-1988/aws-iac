a2c9fdcdae56b3d86f4d3b56f4163851f82628b1
manish-rajput-1988 <manish.rajput.8819@gmail.com>
2025-03-26 21:58:07 +0530

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -12,7 +12,7 @@ variable "networking" {
     cidr_block      = "10.0.0.0/16"
     region          = "us-east-2"
     vpc_name        = "custom-vpc"
-    azs             = ["us-east-1a", "us-east-1b"]
+    azs             = ["us-east-2a", "us-east-2b"]
     public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
     private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
     nat_gateways    = true
