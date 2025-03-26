b27909bd351fb18c6c4fea02dce30cff8c36d80e
manish-rajput-1988 <manish.rajput.8819@gmail.com>
2025-03-26 20:42:34 +0530

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -9,12 +9,12 @@ variable "networking" {
     nat_gateways    = bool
   })
   default = {
-    cidr_block      = "141.0.0.0/16"
-    region          = "eu-central-1"
-    vpc_name        = "terraform-vpc"
-    azs             = ["eu-central-1a", "eu-central-1b"]
-    public_subnets  = ["141.0.1.0/24", "141.0.2.0/24"]
-    private_subnets = ["141.0.3.0/24", "141.0.4.0/24"]
+    cidr_block      = "10.0.0.0/16"
+    region          = "us-east-1"
+    vpc_name        = "custom-vpc"
+    azs             = ["us-east-1a", "us-east-1b"]
+    public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+    private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
     nat_gateways    = true
   }
 }
@@ -103,8 +103,8 @@ variable "node_groups" {
   }))
   default = [
     {
-      name           = "t3-micro-standard"
-      instance_types = ["t3.micro"]
+      name           = "t3-medim-standard"
+      instance_types = ["t3.medium"]
       ami_type       = "AL2_x86_64"
       capacity_type  = "ON_DEMAND"
       disk_size      = 20
@@ -117,21 +117,7 @@ variable "node_groups" {
         max_unavailable = 1
       }
     },
-    {
-      name           = "t3-micro-spot"
-      instance_types = ["t3.micro"]
-      ami_type       = "AL2_x86_64"
-      capacity_type  = "SPOT"
-      disk_size      = 20
-      scaling_config = {
-        desired_size = 2
-        max_size     = 3
-        min_size     = 1
-      }
-      update_config = {
-        max_unavailable = 1
-      }
-    },
+  
   ]
 
 }
