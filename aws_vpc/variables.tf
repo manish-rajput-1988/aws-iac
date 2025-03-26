8353111f06c357e0e85f4a59f4e49317cf6230e0
manish-rajput-1988 <manish.rajput.8819@gmail.com>
2025-03-26 18:16:36 +0530

    Update variables.tf

--- aws_vpc/variables.tf
+++ aws_vpc/variables.tf
@@ -10,9 +10,9 @@ variable "networking" {
   })
   default = {
     cidr_block      = "10.0.0.0/16"
-    region          = "us-east-1"
-    vpc_name        = "custom-vpc"
-    azs             = ["us-east-1a", "us-east-1b"]
+    region          = "us-east-2"
+    vpc_name        = "taplent-non-prod-vpc"
+    azs             = ["us-east-2a", "us-east-2b"]
     public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
     private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
     nat_gateways    = true
@@ -41,7 +41,7 @@ variable "security_groups" {
     }))
   }))
   default = [{
-    name        = "custom-security-group"
+    name        = "taplent-non-prod-security-group"
     description = "Inbound & Outbound traffic for custom-security-group"
     ingress = [
       {
