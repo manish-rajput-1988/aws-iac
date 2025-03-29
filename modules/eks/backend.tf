2d7d5a00637a9071cf1738b2a1ec1b43765e7dc4
manish-rajput-1988 <manish.rajput.8819@gmail.com>
2025-03-29 11:41:36 +0530

    Delete modules/eks/backend.tf

--- modules/eks/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform{
-    backend "s3" {
-        bucket = "bsuite-terraform-state-bucket-non-prod"
-        key = "taplent-non-prod/terraform.tfstate"
-        region = "ap-southeast-1"
-        dynamodb_table = "bsuite-terraform-non-prod-locks"
-        encrypt        = true
-    }
-}
