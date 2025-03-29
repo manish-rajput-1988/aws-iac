d467b7eab1eb7694b8163ec055df49c9d8c9d527
manish-rajput-1988 <manish.rajput.8819@gmail.com>
2025-03-29 14:23:44 +0530

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -8,6 +8,15 @@ variable "networking" {
     private_subnets = list(string)
     nat_gateways    = bool
   })
+  default = {
+    cidr_block      = "10.0.0.0/16"
+    region          = "us-east-2"
+    vpc_name        = "custom-vpc"
+    azs             = ["us-east-2a", "us-east-2b"]
+    public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+    private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+    nat_gateways    = true
+  }
 }
 
 variable "security_groups" {
@@ -31,6 +40,38 @@ variable "security_groups" {
       ipv6_cidr_blocks = list(string)
     }))
   }))
+  default = [{
+    name        = "custom-security-group"
+    description = "Inbound & Outbound traffic for custom-security-group"
+    ingress = [
+      {
+        description      = "Allow HTTPS"
+        protocol         = "tcp"
+        from_port        = 443
+        to_port          = 443
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = null
+      },
+      {
+        description      = "Allow HTTP"
+        protocol         = "tcp"
+        from_port        = 80
+        to_port          = 80
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = null
+      },
+    ]
+    egress = [
+      {
+        description      = "Allow all outbound traffic"
+        protocol         = "-1"
+        from_port        = 0
+        to_port          = 0
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+      }
+    ]
+  }]
 }
 
 variable "cluster_config" {
@@ -38,6 +79,10 @@ variable "cluster_config" {
     name    = string
     version = string
   })
+  default = {
+    name    = "eks-cluster"
+    version = "1.31"
+  }
 }
 
 variable "node_groups" {
@@ -56,6 +101,25 @@ variable "node_groups" {
       max_unavailable = number
     })
   }))
+  default = [
+    {
+      name           = "t3-medim-standard"
+      instance_types = ["t3.medium"]
+      ami_type       = "AL2_x86_64"
+      capacity_type  = "ON_DEMAND"
+      disk_size      = 20
+      scaling_config = {
+        desired_size = 2
+        max_size     = 3
+        min_size     = 1
+      }
+      update_config = {
+        max_unavailable = 1
+      }
+    },
+  
+  ]
+
 }
 
 variable "addons" {
@@ -63,4 +127,23 @@ variable "addons" {
     name    = string
     version = string
   }))
+  default = [
+    {
+      name    = "aws-ebs-csi-driver"
+      version = "v1.41.0-eksbuild.1"
+      
+    },
+    {
+      name    = "vpc-cni"
+      version = "v1.19.2-eksbuild.5"
+    },
+    {
+      name    = "coredns"
+      version = "v1.11.4-eksbuild.2"
+    },
+    {
+      name    = "kube-proxy"
+      version = "v1.31.3-eksbuild.2"
+    }
+  ]
 }
