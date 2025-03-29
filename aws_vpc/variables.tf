5afca31357a127a32d2a1cf71849e7313d48d400
manish-rajput-1988 <manish.rajput.8819@gmail.com>
2025-03-29 10:43:57 +0530

    Update variables.tf with only definition

--- aws_vpc/variables.tf
+++ aws_vpc/variables.tf
@@ -8,15 +8,6 @@ variable "networking" {
     private_subnets = list(string)
     nat_gateways    = bool
   })
-  default = {
-    cidr_block      = "10.0.0.0/16"
-    region          = "us-east-2"
-    vpc_name        = "taplent-non-prod-vpc"
-    azs             = ["us-east-2a", "us-east-2b"]
-    public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-    private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-    nat_gateways    = true
-  }
 }
 
 variable "security_groups" {
@@ -40,36 +31,4 @@ variable "security_groups" {
       ipv6_cidr_blocks = list(string)
     }))
   }))
-  default = [{
-    name        = "taplent-non-prod-security-group"
-    description = "Inbound & Outbound traffic for custom-security-group"
-    ingress = [
-      {
-        description      = "Allow HTTPS"
-        protocol         = "tcp"
-        from_port        = 443
-        to_port          = 443
-        cidr_blocks      = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = null
-      },
-      {
-        description      = "Allow HTTP"
-        protocol         = "tcp"
-        from_port        = 80
-        to_port          = 80
-        cidr_blocks      = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = null
-      },
-    ]
-    egress = [
-      {
-        description      = "Allow all outbound traffic"
-        protocol         = "-1"
-        from_port        = 0
-        to_port          = 0
-        cidr_blocks      = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-      }
-    ]
-  }]
 }
