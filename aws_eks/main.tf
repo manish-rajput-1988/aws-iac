6d62df9d6382914a1cc79d13c2926354f515cd8d
manish-rajput-1988 <manish.rajput.8819@gmail.com>
2025-03-29 12:26:54 +0530

    Update main.tf

--- aws_eks/main.tf
+++ aws_eks/main.tf
@@ -1,13 +1,10 @@
 module "eks" {
   source             = "../modules/eks"
   cluster_config     = var.cluster_config
-  networking         = {
-    public_subnets_id  = module.vpc.public_subnets_id
-    private_subnets_id = module.vpc.private_subnets_id
-  }
-  security_groups_id = module.vpc.security_groups_id
   node_groups        = var.node_groups
-  node_group_role_arn = aws_iam_role.NodeGroupRole.arn
   addons             = var.addons
+  security_groups_id = module.vpc.security_groups_id
+  public_subnets_id  = module.vpc.public_subnets_id
+  private_subnets_id = module.vpc.private_subnets_id
   oidc               = "oidc.eks.us-east-2.amazonaws.com/id/EXAMPLE"
 }
