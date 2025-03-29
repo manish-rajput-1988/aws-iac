285295c8d2643b521c51ee0949459041efbd7d91
manish-rajput-1988 <manish.rajput.8819@gmail.com>
2025-03-29 15:01:25 +0530

    Update main.tf

--- aws_eks/main.tf
+++ aws_eks/main.tf
@@ -1,3 +1,8 @@
 module "eks" {
-  source          = "../modules/eks"
+  source = "./modules/eks"
+  networking      = var.networking
+  security_groups = var.security_groups
+  cluster_config  = var.cluster_config
+  node_groups     = var.node_groups
+  addons          = var.addons
 }
